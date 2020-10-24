9f6b2a436d2b3bc34edae34809b5a2167e6d5ca9
smw3 <smw3@gmx.de>
2020-10-24 13:34:47 +0200

    compile fix

--- fleetview/api/fleet_info.py
+++ fleetview/api/fleet_info.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, request, url_for
 
 from ..fleetview import app
 from ..util.esi.esi_manager import requires_auth, get_auth_url, fetch_access_token
-from ..util.esi.esi_calls import get_char_info, get_fleet_members
+from ..util.esi.esi_calls import get_fleet_members
 from ..util.esi.esi_error import CharacterNotInFleetError
 
 @app.route('/api/fleet')
