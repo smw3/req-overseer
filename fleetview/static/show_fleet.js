343784cac2294a83f1ce3132fbbb625392fb8ef4
smw3 <smw3@gmx.de>
2020-10-25 14:31:29 +0100

    proper sorting

--- fleetview/static/show_fleet.js
+++ fleetview/static/show_fleet.js
@@ -17,67 +17,78 @@ $(document).ready(function(){
 		}
 		var member_table = $('<table>').attr('class','table');
 		member_table.append(
-			$('<tr>').append(
-				$('<th>').text('Pilot').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort'))),
-				$('<th>').text('Ship').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort'))),
-				$('<th>').text('Type').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort')))));
+			$('<thead>').append(
+				$('<tr>').append(
+					$('<th>').text('Pilot').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))),
+					$('<th>').text('Ship').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))),
+					$('<th>').text('Type').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))))));
+							
+		var member_table_body = $('<tbody>');
 		$.each(data["members"], function (index, value) {
 			console.log(index);
 			var row = $('<tr>');
 			row.append($('<td>').text(value["name"]));
 			row.append($('<td>').text(value["ship_info"]["name"]));
 			row.append($('<td>').text(value["ship_info"]["type"]));
-			member_table.append(row);
+			member_table_body.append(row);
 		});
+		member_table.append(member_table_body);
 		console.log("Done!");
 		
 		$("#members").append(member_table);
 		member_table.tablesort();
 		
 		var fleetcomp_table = $('<table>').attr('class','table');
+		var fleetcomp_table_body = $('<tbody>');
 		fleetcomp_table.append(
-			$('<tr>').append(
-				$('<th>').text('Type').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort'))),
-				$('<th>').text('Number').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort')))));
+			$('<thead>').append(
+				$('<tr>').append(
+					$('<th>').text('Type').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))),
+					$('<th>').text('Number').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))))));
 						
 		$.each(data["fleet_comp"], function (index, value) {
 			console.log(index);
 			var row = $('<tr>');
 			row.append($('<td>').text(index));
 			row.append($('<td>').text(value));
-			fleetcomp_table.append(row);
+			fleetcomp_table_body.append(row);
 		});
+		fleetcomp_table.append(fleetcomp_table_body);
 		console.log("Done!");
 		
 		$("#fleetcomp").append(fleetcomp_table);	
 		fleetcomp_table.tablesort();		
 				
 		var ships_table = $('<table>').attr('class','table');
+		var ships_table_body = $('<tbody>');
 		ships_table.append(
-			$('<tr>').append(
-				$('<th>').text('Name').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort'))),
-				$('<th>').text('Number').append(
-					$('<span>').attr("class","icon").append(
-						$('<i>').attr('class','fas fa-sort')))));
+			$('<thead>').append(
+				$('<tr>').append(
+					$('<th>').text('Name').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))),
+					$('<th>').text('Number').append(
+						$('<span>').attr("class","icon").append(
+							$('<i>').attr('class','fas fa-sort'))))));
+							
 		$.each(data["ships"], function (index, value) {
 			console.log(index);
 			var row = $('<tr>');
 			row.append($('<td>').text(index));
 			row.append($('<td>').text(value));
-			ships_table.append(row);
+			ships_table_body.append(row);
 		});
+		ships_table.append(ships_table_body);
 		console.log("Done!");
 		
 		$("#ships").append(ships_table);
